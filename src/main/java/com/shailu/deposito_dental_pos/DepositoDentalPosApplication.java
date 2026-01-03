c5ce9e0c4d93c8c8694c2dc57b1f24ad86500e23
Ega1605 <120615063+Ega1605@users.noreply.github.com>
2026-01-03 15:31:40 -0600

    initial commit

---
+++ src/main/java/com/shailu/deposito_dental_pos/DepositoDentalPosApplication.java
@@ -0,0 +1,13 @@
+package com.shailu.deposito_dental_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositoDentalPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositoDentalPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shailu/deposito_dental_pos/DepositoDentalPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shailu.deposito_dental_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositoDentalPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
