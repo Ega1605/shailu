a8aa03a960920e96ee05b4a28a3804e1d023ba7f
Ega1605 <120615063+Ega1605@users.noreply.github.com>
2026-01-30 17:01:07 -0600

    report schedule

--- src/main/java/com/shailu/deposito_dental_pos/repository/SalesRepository.java
+++ src/main/java/com/shailu/deposito_dental_pos/repository/SalesRepository.java
@@ -1,11 +1,16 @@
 package com.shailu.deposito_dental_pos.repository;
 
 import com.shailu.deposito_dental_pos.model.entity.Sales;
+import com.shailu.deposito_dental_pos.model.projection.SalesByPaymentProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface SalesRepository extends JpaRepository<Sales, Long> {
@@ -29,4 +34,19 @@ public interface SalesRepository extends JpaRepository<Sales, Long> {
             """)
     Page<Sales> findByDeleteDateIsNullAndId(
             Long id, Pageable pageable);
+
+
+    @Query("""
+        SELECT s.paymentType AS paymentType,
+            COUNT(s) AS totalSales,
+            SUM(s.total) AS totalAmount
+        FROM Sales s
+        WHERE s.status = 'COMPLETED'
+          AND s.createdDate BETWEEN :start AND :end
+        GROUP BY s.paymentType
+    """)
+    List<SalesByPaymentProjection> getSalesByPaymentType(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end
+    );
 }
---
+++ src/main/java/com/shailu/deposito_dental_pos/scheduler/ReportsScheduler.java
@@ -0,0 +1,24 @@
+package com.shailu.deposito_dental_pos.scheduler;
+
+import com.shailu.deposito_dental_pos.service.ReportsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReportsScheduler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private ReportsService reportsService ;
+
+    @Scheduled(cron = "0 29 19 * * *", zone = "America/Mexico_City")
+    public void executeDailyReport(){
+        logger.info("Generating daily Excel report...");
+        reportsService.createDailyReport();
+        logger.info("Excel report created");
+    }
+}
